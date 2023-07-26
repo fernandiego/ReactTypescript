849b88dac2e5c0ebb33a4c303d84587e18d2e7b7
fox <fernandodiego.sn@gmail.com>
2023-07-26 19:45:52 -0300

    adicionando itens do form na lista

--- react-typescript-alura/src/App.tsx
+++ react-typescript-alura/src/App.tsx
@@ -1,14 +1,24 @@
-import React from "react"
+import React, {useState} from "react"
 import style from "./style.module.scss"
-import Formulario from "./components/Formulario";
+import Formulario from "./components/Formulario"
 import Lista from "./components/Lista";
 import Cronometro from "./components/Cronometro";
 
 function App() {
+    const [tarefas, setTarefas] = useState([{
+        tarefa: 'React',
+        tempo: '02:00:00'
+    }, {
+        tarefa: 'JavaScript',
+        tempo: '01:00:00'
+    }, {
+        tarefa: 'TypeScript',
+        tempo: '03:00:00'
+    }])
     return (
         <div className={style.AppStyle}>
-            <Formulario/>
-            <Lista/>
+            <Formulario setTarefas={setTarefas} />
+            <Lista tarefas={tarefas}/>
             <Cronometro/>
         </div>
     );
--- react-typescript-alura/src/components/Botao/index.tsx
+++ react-typescript-alura/src/components/Botao/index.tsx
@@ -1,18 +1,19 @@
-import React, { ReactNode } from "react";
 import style from "./Botao.module.scss";
+import React from "react";
 
-interface BotaoProps {
-  children: ReactNode;
+
+class Botao extends React.Component<{
+
+    type?: "button" | "submit" | "reset" | undefined
+    children: React.ReactNode;
+}> {
+    render() {
+        const {type = "button"} = this.props
+        return (
+            <button type={type} className={style.botao}>
+                {this.props.children}
+            </button>
+        );
+    }
 }
-
-class Botao extends React.Component<BotaoProps> {
-  render() {
-    return (
-      <button className={style.botao}>
-        {this.props.children}
-      </button>
-    );
-  }
-}
-
 export default Botao;
