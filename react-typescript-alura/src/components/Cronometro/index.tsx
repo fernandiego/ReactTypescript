7f0df97be4407423bcb96434dbf03d6a8cdcf488
fox <fernandodiego.sn@gmail.com>
2023-07-28 20:32:57 -0300

    Implementando o UseEffect

--- react-typescript-alura/src/components/Cronometro/index.tsx
+++ react-typescript-alura/src/components/Cronometro/index.tsx
@@ -3,17 +3,23 @@ import Relogio from "./Relogio";
 import style from './Cronometro.module.scss';
 import {tempoParaSegundos} from "../../common/utils/time";
 import {ITarefa} from "../../types/tarefas";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 interface Props {
     selecionado: ITarefa | undefined
 }
 
 export default function Cronometro({selecionado}: Props) {
-    const [tempo, setTempo] = useState<number>();
-    if(selecionado?.tempo) {
-        setTempo(tempoParaSegundos(selecionado.tempo));
-    }
+
+    const [tempo, setTempo] = useState<number>()
+
+    useEffect(() => {
+        if (selecionado?.tempo) {
+        setTempo(tempoParaSegundos(selecionado.tempo))
+
+        }
+    },[selecionado])
+
     return (
         <div className={style.cronometro}>
             <p className={style.titulo}>Escolha um card e inicie o Cronômetro</p>
