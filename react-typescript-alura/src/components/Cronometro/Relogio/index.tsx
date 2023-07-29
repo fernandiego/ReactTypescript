b9607b39a1ee07444e174c3ca09af986546ae19f
fox <fernandodiego.sn@gmail.com>
2023-07-29 14:48:29 -0300

    Agora ao clicar em um item da lista, os minutos aparecem no cronometro

--- react-typescript-alura/src/components/Cronometro/Relogio/index.tsx
+++ react-typescript-alura/src/components/Cronometro/Relogio/index.tsx
@@ -1,13 +1,27 @@
 import style from './Relogio.module.scss';
+import {types} from "sass";
+import String = types.String;
 
-export default function Relogio() {
-  return (
-    <>
-      <span className={style.relogioNumero}>0</span>
-      <span className={style.relogioNumero}>0</span>
-      <span className={style.relogioDivisao}>:</span>
-      <span className={style.relogioNumero}>0</span>
-      <span className={style.relogioNumero}>0</span>
-    </>
-  )
+interface Props {
+    tempo: number | undefined
+}
+
+export default function Relogio({tempo = 0}: Props) {
+    const minutos = Math.floor(tempo / 60)
+    const segundos = tempo % 60
+    // @ts-ignore
+    const [minutoDezena, minutoUnidade] = minutos.toString().padStart(2, '0');
+    // @ts-ignore
+    const [segundoDezena, segundoUnidade] = segundos.toString().padStart(2, '0');
+
+
+    return (
+        <>
+            <span className={style.relogioNumero}>{minutoDezena}</span>
+            <span className={style.relogioNumero}>{minutoUnidade}</span>
+            <span className={style.relogioDivisao}>:</span>
+            <span className={style.relogioNumero}>{segundoDezena}</span>
+            <span className={style.relogioNumero}>{segundoUnidade}</span>
+        </>
+    )
 }
