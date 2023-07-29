6abae1b78a7f99e81b5e1dcd5dd3b21654f5d35c
fox <fernandodiego.sn@gmail.com>
2023-07-29 16:22:58 -0300

    Algumas refatorações para simplificar o código

--- react-typescript-alura/src/components/Botao/index.tsx
+++ react-typescript-alura/src/components/Botao/index.tsx
@@ -1,21 +1,18 @@
 import style from "./Botao.module.scss";
 import React from "react";
 
-
-class Botao extends React.Component<{
-
+interface Props {
     type?: "button" | "submit" | "reset" | undefined,
     onCLick?: () => void,
-    children: React.ReactNode;
-}> {
-    render() {
-        const {type = "button", onCLick} = this.props
-        return (
-            <button onClick={onCLick} type={type} className={style.botao}>
-                {this.props.children}
-            </button>
-        );
-    }
+    children?: React.ReactNode;
+}
+
+function Botao({onCLick, type, children}: Props) {
+    return (
+        <button onClick={onCLick} type={type} className={style.botao}>
+            {children}
+        </button>
+    );
 }
 
 export default Botao;
